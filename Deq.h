0f0415b783b34a78122b7c0c045231a17f61388d
Lichuza <38728937+Lichuza@users.noreply.github.com>
2018-05-10 22:17:17 +0300

    Add files via upload

---
+++ Deq.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include<stdlib.h>
+
+//Ёлемент списка
+struct Node
+{
+	int x;
+	Node *next, *prev;
+};
+
+class Deq
+{
+	
+public:
+	// онструктор по умолчанию
+	Deq():head(NULL), tail(NULL){}
+	~Deq();
+
+	void display();
+	void input(int x);
+	void del(int x);
+	bool verify();
+private:
+	long int count;
+	Node *head, *tail;
+};
+
---
+++ m.cpp
@@ -0,0 +1,43 @@
+﻿#include<stdio.h>
+#include<iostream>
+#include<locale>
+#include"Deq.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int i = 0,x,t;
+	Deq d;
+
+		while (true)
+		{
+			cout << "¬ведите "<< i << " элемент списка: \n";
+			cout << "¬ведите '0' дл¤ завершени¤ заполнени¤ списка\n";
+			cin >> x;
+			if (x == 0) break;
+			d.input(x);
+			i++;
+		}
+
+		if (d.verify() == true)
+		{
+			cout << "DEQUE\n";
+			d.display();
+
+			while (true)
+			{
+				cout << "¬ведите элемент, который хотите удалить\n";
+				cout << "¬ведите '0' дл¤ выхода\n";
+				cin >> x;
+				if (x == 0) break;
+				d.del(x);
+			}
+
+			cout << "DEQUE\n";
+			d.display();
+		}
+
+	system("pause");
+	return 0;
+}
